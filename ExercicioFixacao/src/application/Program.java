c963e504a72eb766e90a640d0ffb50d28fcbda08
Jean Crus <jean.carlos@pucpr.edu.br>
2019-02-11 17:44:10 -0200

    Aula 202 - Exercicio fixação pipeline, reduce,map, filter, predicate

--- ExercicioFixacao/src/application/Program.java
+++ ExercicioFixacao/src/application/Program.java
@@ -3,41 +3,53 @@ package application;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
-import entities.Candidate;
+import entities.Employee;
 
 public class Program {
 
 	public static void main(String[] args) {
-			Scanner sc = new Scanner(System.in);
-			System.out.print("Enter file full path: ");
-			String path = sc.nextLine();
-			Map<Candidate,Integer> votes = new LinkedHashMap<>();
-			try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-
-				String line = br.readLine();
-				while (line != null) {
-					String[] fields = line.split(",");
-					String name = fields[0];
-					int quantity = Integer.parseInt(fields[1]);
-					Candidate ps = new Candidate(name);
-					if (votes.containsKey(ps)) {
-						int count = votes.get(ps);
-						votes.put(ps, quantity + count);
-					} else {
-						votes.put(ps, quantity);
-					}
-					line = br.readLine();
-				}
-			} catch (IOException e) {
-				System.out.println("Error: " + e.getMessage());
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter full file path: ");
+		String path = sc.nextLine();
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			
+			List<Employee> list = new ArrayList<>();
+			
+			String line = br.readLine();
+			while (line != null) {
+				String[] fields = line.split(",");
+				list.add(new Employee(fields[0], fields[1], Double.parseDouble(fields[2])));
+				line = br.readLine();
 			}
-			for (Candidate key : votes.keySet()) {
-				System.out.println(key + ": " + votes.get(key));
-			}
-			sc.close();
+			
+			Comparator<String> comp = (s1, s2) -> s1.toUpperCase().compareTo(s2.toUpperCase());
+			double min = 2000.00;
+			
+			double sum = list.stream()
+					.filter(p -> p.getName().charAt(0) == 'M')
+					.map(p -> p.getSalary())
+					.reduce(0.0, (x,y) -> x + y);
+			
+			List<String> email = list.stream()
+					.filter(p -> p.getSalary() > min)
+					.map(p -> p.getEmail())//map para pegar somente os nomes dos produtos com preço abaixo da média
+					.sorted(comp)//comparator para ordernar a lista em ordem alfabética
+					.collect(Collectors.toList()); //necessário converter a stream em lista
+					email.forEach(System.out::println);
+			System.out.println("Sum of salary of people whose name starts with 'M': " + String.format("%.2f", sum));
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		sc.close();
 	}
 }
